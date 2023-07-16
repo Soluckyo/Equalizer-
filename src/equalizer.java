276653615c0b99fbf84ed118f00ad9ff5aa52779
Soluckyo <twinki0000@yandex.ru>
2023-07-16 16:13:49 +0300

    add graphics

--- src/equalizer.java
+++ src/equalizer.java
@@ -1,43 +1,82 @@
 import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
 
-public class equalizer implements ControllerEventListener{
+public class equalizer{
+    static JFrame f = new JFrame("Equalizer");
+    static drawPanel ml;
+
     public static void main(String[] args) {
         equalizer eq = new equalizer();
         eq.go();
     }
-    void go(){
-        try{
+
+    public void setUpGUI() {
+        ml = new drawPanel();
+        f.setContentPane(ml);
+        f.setBounds(30, 30, 300, 300);
+        f.setVisible(true);
+    }
+
+    public void go() {
+        setUpGUI();
+        try {
             Sequencer sequencer = MidiSystem.getSequencer();
             sequencer.open();
 
-            int[] eventsIWant = {127};
-            sequencer.addControllerEventListener(this, eventsIWant);
+            sequencer.addControllerEventListener(ml, new int[]{127});
 
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
-
-            for (int i = 5; i < 61; i+=4){
-                track.add(makeEvent(144,1, i,100, i));
-
-                track.add(makeEvent(176,1,127,0, i));
-
-                track.add(makeEvent(128,1, i,100,i+2));
+            int r = 0;
+            for (int i = 0; i < 60; i += 4) {
+                r = (int) ((Math.random() * 50) + 1);
+                track.add(makeEvent(144, 1, r, 100, i));
+                track.add(makeEvent(176, 1, 127, 0, i));
+                track.add(makeEvent(128, 1, r, 100, i + 2));
             }
             sequencer.setSequence(seq);
             sequencer.setTempoInBPM(220);
             sequencer.start();
-        }catch(Exception ex) {ex.printStackTrace();}
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
-    public void controlChange(ShortMessage event){
-        System.out.println("Lya");
-    }
-    public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick){
+    public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
         MidiEvent event = null;
-        try{
+        try {
             ShortMessage a = new ShortMessage();
             a.setMessage(comd, chan, one, two);
             event = new MidiEvent(a, tick);
-        }catch(Exception e){}
+        } catch (Exception e) {
+        }
         return event;
+    }
+
+    class drawPanel extends JPanel implements ControllerEventListener {
+        boolean msg = false;
+        public void controlChange(ShortMessage event) {
+            msg = true;
+            repaint();
+        }
+        public void paintComponent(Graphics g){
+            if (msg){
+                Graphics2D g2 = (Graphics2D) g;
+                int red = (int) (Math.random()*250);
+                int green = (int) (Math.random()*250);
+                int blue = (int) (Math.random()*250);
+
+                g.setColor(new Color(red, green, blue));
+
+                int height = (int) ((Math.random()*120)+10);
+                int width = (int)((Math.random()*120)+10);
+
+                int x = (int)((Math.random()*40)+10);
+                int y = (int)((Math.random()*40)+10);
+
+                g.fillRect(x,y,height,width);
+                msg = false;
+            }
+        }
     }
 }
